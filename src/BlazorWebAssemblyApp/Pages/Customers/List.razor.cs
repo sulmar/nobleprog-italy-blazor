31fc22f5b75e9918f39b06c3f6c251af4c18553a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-03-26 09:07:11 +0000

    Add Loader Component with ChildContent

---
+++ src/BlazorWebAssemblyApp/Pages/Customers/List.razor.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+
+namespace BlazorWebAssemblyApp.Pages.Customers;
+
+public partial class List
+{
+    // This is equivalent to: @inject ICustomerService Api (declared at the top of this .razor file).
+    // [Inject]
+    // public ICustomerService Api { get; set; }
+
+
+    private bool isLoading => customers is null;
+
+    private IEnumerable<Customer>? customers;
+    
+
+    protected override async Task OnInitializedAsync()
+    {
+        customers = await Api.GetAll();
+    }
+}
