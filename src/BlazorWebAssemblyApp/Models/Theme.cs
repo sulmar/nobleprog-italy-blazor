7bdc8888e376f0c936c1261062d7f4314385b275
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-03-26 11:23:10 +0000

    Add Theme with Parameters

---
+++ src/BlazorWebAssemblyApp/Models/Theme.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWebAssemblyApp.Models;
+
+public class Theme
+{
+    public string Name { get; set; } = "Light";
+    public string Background { get; set; } = "white";
+    public string TextColor { get; set; } = "black";
+}
